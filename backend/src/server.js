bfc04383d7b0cb7fd5235578e29d578d6bfe18c8
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2025-11-04 14:07:25 +0530

    Setup Database and tools

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
-const __dirname=path.resolve()
+const __dirname=path.resolve();
 
 app.get("/health", (req, res) => {
     res.status(200).json({message: "Success from Backend"})
@@ -17,6 +18,16 @@ if (ENV.NODE_ENV === "production") {
     app.get("/{*any}", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
-}
+};
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+    } catch (error) {
+        console.error("Error starting the server", error)
+    }
+};
+
+startServer();
