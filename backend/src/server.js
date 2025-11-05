4363a369aec161e792f41bab2d549d27c94f2719
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2025-11-05 20:18:57 +0530

    Auth and DB synced

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,34 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+
+import { inngest, functions } from "./lib/inngest.js";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
 const __dirname=path.resolve();
 
+// Middlewares
+app.use(express.json());
+// credentials: true meaning ?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get("/health", (req, res) => {
-    res.status(200).json({message: "Success from Backend"})
-})
+    res.status(200).json({message: "Success from Backend"});
+});
 
 // Make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 };
 
 
@@ -26,7 +37,7 @@ const startServer = async () => {
         await connectDB();
         app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
     } catch (error) {
-        console.error("Error starting the server", error)
+        console.error("Error starting the server", error);
     }
 };
 
