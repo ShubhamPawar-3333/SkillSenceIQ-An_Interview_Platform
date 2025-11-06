bfca982262e87960127095bfbbdaf883a56bd2e5
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2025-11-06 21:28:19 +0530

    Server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,9 @@ import { inngest, functions } from "./lib/inngest.js";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
@@ -15,12 +18,19 @@ const __dirname=path.resolve();
 app.use(express.json());
 // credentials: true meaning ?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}));
+app.use(clerkMiddleware()); // This adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chats", chatRoutes);
 
 app.get("/health", (req, res) => {
     res.status(200).json({message: "Success from Backend"});
 });
+
+// When you pass an array of middleware to express, it automatically flattens and executes them sequentially, one by one
+// app.get("/video-calls", protectRoute, (req, res) => {
+//     res.status(200).json({message: "This is a protected route"});
+// });
 
 // Make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
